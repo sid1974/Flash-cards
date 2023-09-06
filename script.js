d5fb401bec6b41bd05868ddb6d1926c8bdefc446
Siddarth <116632503+sid1974@users.noreply.github.com>
2023-09-06 21:54:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const flashcards = document.getElementsByClassName("flashcards")[0];
+const createbox = document.getElementsByClassName("create-box")[0];
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+
+let contentArray = localStorage.getItem('items') ? JSON.parse( localStorage.getItem('items')) :[];
+
+
+
+function showCreateCarBox(){
+    createbox.style.display = 'block';
+}
+
+function delFlashcards(){
+    localStorage.clear();
+    flashcards.innerHTML = '';
+    contentArray = [];
+}
+
+function hidecreatebox(){
+    createbox.style.display = 'none';
+}
+
+
+
+function addflashcard(){
+    var flashcard_info = {
+        'my_question' : question.value,
+        'my_answer' : answer.value,
+    }
+
+    contentArray.push(flashcard_info);
+    localStorage.setItem('item',JSON.stringify (contentArray));
+    divMaker(contentArray[contentArray.length - 1]);
+    question.value = '';
+    answer.value = '';
+}
+
+contentArray.forEach(divMaker);
+function divMaker(text){
+    var div = document.createElement("div");
+    var h2_question = document.createElement("h2");
+    var h2_answer = document.createElement("h2");
+
+
+    div.className = 'flashcard';
+
+    h2_question.setAttribute('style',"border-top:1px solid red; padding:15px;margin-top:30px;");
+    h2_question.innerHTML =text.my_question;
+
+
+    h2_answer.setAttribute('style',"text-align:center;display:none;color:red");
+    h2_answer.innerHTML =text.my_answer;
+
+    div.appendChild(h2_question);
+    div.appendChild(h2_answer);
+
+    div.addEventListener("click",function(){
+        if(h2_answer.style.display =="none")
+            h2_answer.style.display ='block';
+        else
+            h2_answer.style.display ="none";
+    })
+
+    flashcards.appendChild(div);
+}
+
